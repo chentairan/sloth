f9c023e2fb000132729d54eaa7b1d8d7b3eb4175
WangChao <jpwc@qq.com>
2016-06-02 11:20:26 +0800

    update

---
+++ myconfig.py
@@ -0,0 +1,8 @@
+LABELS = (
+    {"attributes": {"class": "FacePoint"},
+     "item":     "sloth.items.PointItem",
+     "inserter": "sloth.items.PointItemInserter",
+     "text":     "Points",
+     'hotkey':   'f'
+    },
+)
